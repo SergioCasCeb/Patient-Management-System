db0e62229cf80ac0ccbb1222fcae13e75a001e2c
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2022-12-21 12:25:55 +0100

    services loaded and error handling

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -8,25 +8,16 @@ userIcon.addEventListener("click", () => {
 })
 
 
-/* sections */
+/* section patients */
 const patientTab = document.querySelector(".pat-tab");
-const serviceTab = document.querySelector(".service-tab");
 const patientSection = document.querySelector(".patient-main");
-const serviceSection = document.querySelector(".service-main");
 
 patientTab.addEventListener("click", () => {
     patientTab.classList.add("selected");
     serviceTab.classList.remove("selected");
     patientSection.classList.remove("hidden");
     serviceSection.classList.add("hidden");
-
-})
-serviceTab.addEventListener("click", () => {
-    serviceTab.classList.add("selected");
-    patientTab.classList.remove("selected");
-    serviceSection.classList.remove("hidden");
-    patientSection.classList.add("hidden");
-    
+    loadPatients();
 })
 
 /**** Main functionality patient section****/
@@ -38,7 +29,6 @@ const newPatForm = document.querySelector(".new-pat-form");
 const submitBtnPat = newPatForm.querySelector(".submit-btn");
 const cancelBtnPat = newPatForm.querySelector(".cancel-btn");
 const newPatBtn = document.querySelector(".new-pat-btn");
-
 
 loadPatients();
 
@@ -53,8 +43,9 @@ function returnPatDefault(){
 }
 //Function to load patients table
 function loadPatients() {
-    //console.log("the patients have loaded");
+    returnPatDefault();
     filterPatient.options[0].selected= true;
+    searchPatient.disabled = true;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'patients.json', true);
     xhr.onload = function() {
@@ -84,10 +75,11 @@ function loadPatients() {
         const rows = tablePatients.querySelectorAll('tr:not(tr.no-results)');
 
         filterPatient.addEventListener("change", () => {
-            var filterVal = filterPatient.options[filterPatient.selectedIndex].value;
+            let filterVal = filterPatient.options[filterPatient.selectedIndex].value;
+            searchPatient.disabled = false;
             returnPatDefault();
             searchPatient.addEventListener("keyup", () => {
-                var value = searchPatient.value.toLowerCase();
+                let value = searchPatient.value.toLowerCase();
                 rows.forEach(row => {
                     if(row.querySelector(`td:nth-child(${filterVal})`).textContent.toLowerCase().includes(value)){
                         row.classList.add("shown");
@@ -118,3 +110,97 @@ cancelBtnPat.addEventListener("click", () => {
 newPatBtn.addEventListener("click", () => {
     newPatForm.classList.remove("hidden");
 })
+
+
+
+/* section services */
+const serviceTab = document.querySelector(".service-tab");
+const serviceSection = document.querySelector(".service-main");
+
+/**** Services selection****/
+const tableServices = document.querySelector(".table-services tbody");
+const searchService = document.querySelector(".search-service input");
+const filterService = document.querySelector(".filter-service select");
+const noResultsSer = document.querySelector(".table-services .no-results");
+//const newPatForm = document.querySelector(".new-pat-form");
+//const submitBtnPat = newPatForm.querySelector(".submit-btn");
+//const cancelBtnPat = newPatForm.querySelector(".cancel-btn");
+//const newPatBtn = document.querySelector(".new-pat-btn");
+
+//Open and show the services tab
+serviceTab.addEventListener("click", () => {
+    serviceTab.classList.add("selected");
+    patientTab.classList.remove("selected");
+    serviceSection.classList.remove("hidden");
+    patientSection.classList.add("hidden");  
+    loadServices();
+})
+//return service tab to defaults
+function returnSerDefault(){
+    const rows = tableServices.querySelectorAll('tr:not(tr.no-results)');
+    searchService.value = '';
+    noResultsSer.classList.add("hidden");
+    rows.forEach(row => {
+        row.classList.add("shown");
+        row.style.display = 'table-row';
+    })
+}
+//Function to load services table
+function loadServices() {
+    returnSerDefault();
+    searchService.disabled = true;
+    filterService.options[0].selected= true;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'services.json', true);
+    xhr.onload = function() {
+        if(this.status == 200){
+            var services = JSON.parse(this.responseText);
+            //console.log(services);
+            for(var i in services){
+                let tr = document.createElement("tr");
+                tr.classList.add("hover:shadow-gray-blue-200", "hover:shadow-md", "duration-300", "rounded-lg");
+                tr.innerHTML = `
+                <td class="border-y-2 border-l-2 rounded-l-lg border-gray-blue-400 py-2">${services[i].idSer}</td>
+                <td class="border-y-2 border-gray-blue-400 py-2">${services[i].name}</td>
+                <td class="border-y-2 border-gray-blue-400 py-2">${services[i].privatePrice}</td>
+                <td class="border-y-2 border-gray-blue-400 py-2">${services[i].pvPrice}</td>
+                <td class="border-y-2 border-r-2 rounded-r-lg border-gray-blue-400 flex flex-row justify-center items-center py-2">
+                    <button class="mr-2 group"><i class="fa-solid fa-file-pen py-1 px-2 text-xl group-hover:text-primary-600 duration-300"></i></button>
+                    <button class="group"><i class="fa-solid fa-trash-can py-1 px-2 text-xl group-hover:text-primary-600 duration-300"></i></button>
+                </td>
+                `;
+                tableServices.append(tr); 
+            }
+        }
+
+        const rows = tableServices.querySelectorAll('tr:not(tr.no-results)');
+
+        filterService.addEventListener("change", () => {
+            searchService.disabled = false;
+            let filterVal = filterService.options[filterService.selectedIndex].value;
+            returnSerDefault();
+            searchService.addEventListener("keyup", () => {
+                let value = searchService.value.toLowerCase();
+                rows.forEach(row => {
+                    if(row.querySelector(`td:nth-child(${filterVal})`).textContent.toLowerCase().includes(value)){
+                        row.classList.add("shown");
+                        row.style.display = 'table-row';
+                    }
+                    else{
+                        row.style.display = 'none';
+                        row.classList.remove("shown");
+                    }
+                });
+
+                let shownRows = document.querySelectorAll(".table-services tbody .shown");
+                if(shownRows.length == 0){
+                    noResultsSer.classList.remove("hidden");
+                }
+                else{
+                    noResultsSer.classList.add("hidden");
+                }
+            }) 
+        })
+    }
+    xhr.send();
+}
